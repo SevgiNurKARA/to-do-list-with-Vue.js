ca270100133045cc7f62d6365fcdcad2c9d34182
SevgiNurKARA <112270251+SevgiNurKARA@users.noreply.github.com>
2024-07-06 06:17:42 +0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+ <div id="app">
+   
+    <Todo />
+  </div>
+</template>
+
+<script>
+import './components/style.css';
+import Todo from './components/Todo.vue';
+
+export default{
+name: 'AnotherComponent',
+components: {
+    Todo
+  },
+}
+
+</script>
+
+<style>
+
+</style>
